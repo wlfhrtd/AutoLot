5959c2b9ed37073dfaee44fea2a5ab69c68511af
slrnd <strmbld@gmail.com>
2022-07-12 12:12:35 +0300

    initial config startup and db

--- AutoLot.Api/Startup.cs
+++ AutoLot.Api/Startup.cs
@@ -11,15 +11,25 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using AutoLot.Dal.EfStructures;
+using AutoLot.Dal.Initialization;
+using AutoLot.Dal.Repository;
+using AutoLot.Dal.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace AutoLot.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             // possible to inject env and logger
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
@@ -27,8 +37,24 @@ namespace AutoLot.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                    options.JsonSerializerOptions.WriteIndented = true;
+                });
 
-            services.AddControllers();
+            string connectionString = Configuration.GetConnectionString("AutoLot");
+            services.AddDbContextPool<ApplicationDbContext>(
+                options => options.UseSqlServer(connectionString,
+                    sqlOptions => sqlOptions.EnableRetryOnFailure().CommandTimeout(60)));
+            // ApplicationDbContext config
+            services.AddScoped<ICarRepository, CarRepository>();
+            services.AddScoped<ICreditRiskRepository, CreditRiskRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IMakeRepository, MakeRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AutoLot.Api", Version = "v1" });
@@ -36,11 +62,18 @@ namespace AutoLot.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // init dev db
+                if (Configuration.GetValue<bool>("RebuildDataBase"))
+                {
+                    SampleDataInitializer.InitializeData(context);
+                }
+
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AutoLot.Api v1"));
             }
--- AutoLot.Mvc/Startup.cs
+++ AutoLot.Mvc/Startup.cs
@@ -8,14 +8,25 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AutoLot.Dal.EfStructures;
+using AutoLot.Dal.Initialization;
+using AutoLot.Dal.Repository;
+using AutoLot.Dal.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace AutoLot.Mvc
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            // possible to inject env and logger
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
@@ -24,20 +35,36 @@ namespace AutoLot.Mvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            string connectionString = Configuration.GetConnectionString("AutoLot");
+            services.AddDbContextPool<ApplicationDbContext>(
+                options => options.UseSqlServer(connectionString,
+                    sqlOptions => sqlOptions.EnableRetryOnFailure().CommandTimeout(60)));
+            // ApplicationDbContext config
+            services.AddScoped<ICarRepository, CarRepository>();
+            services.AddScoped<ICreditRiskRepository, CreditRiskRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IMakeRepository, MakeRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                // init dev db
+                if (Configuration.GetValue<bool>("RebuildDataBase"))
+                {
+                    SampleDataInitializer.InitializeData(context);
+                }
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts(); // HTTP Strict Transport Security Protocol
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -48,9 +75,13 @@ namespace AutoLot.Mvc
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
+
+                /**
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                */
             });
         }
     }
