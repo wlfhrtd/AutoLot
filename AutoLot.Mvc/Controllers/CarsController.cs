f6c643605a037812b8590564180550200b7bfa30
slrnd <strmbld@gmail.com>
2022-07-13 20:01:34 +0300

    quick look on tag helpers

---
+++ AutoLot.Mvc/Controllers/CarsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoLot.Mvc.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
