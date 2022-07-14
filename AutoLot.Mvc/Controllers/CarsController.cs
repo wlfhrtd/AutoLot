ece2c520c877a9719303357da453ddcc09fa480f
slrnd <strmbld@gmail.com>
2022-07-14 13:24:09 +0300

    add Cars controller and views implementation

--- AutoLot.Mvc/Controllers/CarsController.cs
+++ AutoLot.Mvc/Controllers/CarsController.cs
@@ -3,35 +3,143 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoLot.Dal.Repository.Interfaces;
+using AutoLot.Model.Entities;
+using AutoLot.Services.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace AutoLot.Mvc.Controllers
 {
     [Route("[controller]/[action]")]
     public class CarsController : Controller
     {
+        private readonly ICarRepository _carRepository;
+        private readonly IAppLogging<CarsController> _logger;
+
+
+        public CarsController(ICarRepository carRepository, IAppLogging<CarsController> logger)
+        {
+            _carRepository = carRepository;
+            _logger = logger;
+        }
+
+
+        [Route("/[controller]")]
+        [Route("/[controller]/[action]")]
         public IActionResult Index()
         {
+            return View(_carRepository.FindAllIgnoreQueryFilters());
+        }
+
+        [HttpGet]
+        public IActionResult Create([FromServices] IMakeRepository makeRepository)
+        {
+            ViewData["MakeId"] = GetMakes(makeRepository);
+
             return View();
         }
 
-        public IActionResult Create()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([FromServices] IMakeRepository makeRepository, Car car)
         {
+            if (ModelState.IsValid)
+            {
+                _carRepository.Add(car);
+
+                return RedirectToAction(nameof(Details), new { id = car.Id });
+            }
+            
+            ViewData["MakeId"] = GetMakes(makeRepository);
+
             return View();
         }
 
-        public IActionResult Details()
+        [HttpGet("{id?}")]
+        public IActionResult Details(int? id)
         {
-            return View();
+            if (!id.HasValue)
+            {
+                return BadRequest();
+            }
+
+            Car car = GetOne(id);
+
+            return car == null ? NotFound() : View(car);
         }
 
-        public IActionResult Delete()
+        [HttpGet("{id?}")]
+        public IActionResult Delete(int? id)
         {
-            return View();
+            Car car = GetOne(id);
+
+            return car == null ? NotFound() : View(car);
         }
 
-        public IActionResult Edit()
+        [HttpPost("{id}")]
+        public IActionResult Delete(int id, Car car)
         {
-            return View();
+            if (id!=car.Id)
+            {
+                return BadRequest();
+            }
+
+            _carRepository.Remove(car);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("{id?}")]
+        public IActionResult Edit([FromServices] IMakeRepository makeRepository, int? id)
+        {
+            Car car = GetOne(id);
+            if (car == null)
+            {
+                return NoContent();
+            }
+
+            ViewData["MakeId"] = GetMakes(makeRepository);
+
+            return View(car);
+        }
+
+        [HttpPost("{id}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromServices] IMakeRepository makeRepository, int id, Car car)
+        {
+            if (id != car.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _carRepository.Update(car);
+                return RedirectToAction(nameof(Details), new { id = car.Id });
+            }
+
+            ViewData["MakeId"] = GetMakes(makeRepository);
+
+            return View(car);
+        }
+
+        [HttpGet("/[controller]/[action]/{makeId}/{makeName}")]
+        public IActionResult ByMake(int makeId, string makeName)
+        {
+            ViewBag.MakeName = makeName;
+
+            return View(_carRepository.FindAllBy(makeId));
+        }
+
+        internal Car GetOne(int? id)
+        {
+            return !id.HasValue ? null : _carRepository.FindOneById(id.Value);
+        }
+
+        internal SelectList GetMakes(IMakeRepository makeRepository)
+        {
+            return new(makeRepository.FindAll(), nameof(Make.Id), nameof(Make.Name));
         }
     }
 }
