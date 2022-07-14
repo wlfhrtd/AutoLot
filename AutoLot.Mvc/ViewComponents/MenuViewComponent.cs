cbe2a5cfad6c0572a6a8b41c43d6752f522beaec
slrnd <strmbld@gmail.com>
2022-07-14 13:53:59 +0300

    add MenuViewComponent for makes list in nav menu

---
+++ AutoLot.Mvc/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoLot.Dal.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+
+namespace AutoLot.Mvc.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly IMakeRepository _makeRepository;
+
+
+        public MenuViewComponent(IMakeRepository makeRepository)
+        {
+            _makeRepository = makeRepository;
+        }
+
+
+        /*
+         * put templates into any of these:
+         * 
+         * Views/<controller>/Components/<view_component_name>/<view_name>
+           Views/Shared/Components/<view_component_name>/<view_name>
+           Pages/Shared/Components/<view_component_name>/<view_name>
+         * 
+         */
+        public IViewComponentResult Invoke()
+        {
+            var makes = _makeRepository.FindAll().ToList();
+
+            return makes.Any() ? View("MenuView", makes) : new ContentViewComponentResult("Unable to get makes");
+        }
+    }
+}
